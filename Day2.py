cdb7ef5e2ef10398577d392c13cfde995a0fc308
thien0709 <thien070904@gmail.com>
2023-06-16 02:27:13 +0700

    update

---
+++ Day2.py
@@ -0,0 +1,18 @@
+name = "Toi ten la Bach Xuan Thien"
+a =258
+print(name)
+print(len(name)) #Độ dài chuỗi
+print(int('10')) #Chuyển thành số
+print(float(10)) #Chuyển thành float
+print(type(str(1000023479847)))
+userName = input('Enter userName: ') #Nhập dữ liệu từ bàn phím
+list()
+dict()
+min()
+max()
+sum()
+sorted()
+open()
+file()
+help()
+dir()
