9ff1cbf960908da2284d1cec2eb9c78493eda1a8
thien0709 <thien070904@gmail.com>
2023-06-15 23:57:40 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+print("Hello World")
+#this is comment  
+print(5+3)
+print(5/3)
+print(5*3)
+print(5%3)
+print(5//3)
+print(5**3)
+print(5-3)
+"""This is multiline comment
+multiline comment takes multiple lines.
+python is eating the world
+"""
+a = True
+print(a)
+#this is list
+number = [0,4,2,334,4]
+#this is object
+author = {
+    'name': "Xuan Thien",
+    'age': 18,
+    'school' : "OU",
+    'alone' : True
+}
+print(author['name'])
+#Check data type 
+print(type(a))
