68e6503440c9ebf8d3571c0ff20e2e0d388f8230
thien0709 <thien070904@gmail.com>
2023-06-21 10:38:19 +0700

    update

--- Test.py
+++ Test.py
@@ -1,5 +1,10 @@
-import numpy as np
-_A = [[1,2,3],[4,5,6,7],[7,8,9]]
-B = np.array(_A)
-print(B)
-print(_A)
+import pandas as pd
+melbourne_file_path = './melb_data.csv'
+a = pd.read_csv(melbourne_file_path)
+print(a.describe()) #Mô tả số liệu về bảng
+print(a.columns) # (Các tiêu đề của bảng) Xem danh sách tất cả các cột trong tập dữ liệu
+print(a.dropna) # Loại bỏ các các dòng hoặc cột chứa giá trị NaN
+print(a.Address) # Chọn mục tiêu dự đoán (địa chỉ)
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+print(a[melbourne_features].describe()) # Lựa chọn các tính năng (Các cột được nhập vào sau đó được sử dụng để đưa ra dự đoán)
+print(a[melbourne_features].head())
