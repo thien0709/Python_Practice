156044bab298be6b3cdd1c4c74ffeffa3a6dce7b
thien0709 <thien070904@gmail.com>
2023-06-21 08:48:59 +0700

    update numpy

---
+++ Test.py
@@ -0,0 +1,5 @@
+import numpy as np
+_A = [[1,2,3],[4,5,6,7],[7,8,9]]
+B = np.array(_A)
+print(B)
+print(_A)
--- panda.py
+++
@@ -1,2 +0,0 @@
-import pandas as pd 
-price_melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.cvc';
