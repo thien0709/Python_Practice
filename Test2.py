ca697980913d7aa63b789bf97680056e98fd5b75
Bạch Xuân Thiên <thien070904@gmail.com>
2023-06-26 00:22:06 +0700

    update

--- Test2.py
+++ Test2.py
@@ -1,4 +1,20 @@
-import pandas as pd 
-df = pd.read_csv('train.csv')
-print(df.columns)
-print(df.info())
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
+# Đọc dữ liệu từ file CSV
+melbourne_file_path = './melb_data.csv'
+a = pd.read_csv(melbourne_file_path)
+data = a.dropna()
+# Chọn các đặc trưng (features) và nhãn (labels)
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = data[melbourne_features]
+y = data['Price']
+
+# Khởi tạo mô hình DecisionTreeRegressor
+model = DecisionTreeRegressor(random_state=1)
+
+# Fit mô hình vào dữ liệu
+model.fit(X, y)
+# Dự đoán giá nhà cho các mẫu huấn luyện
+predictions = model.predict(X)
+print(predictions)
